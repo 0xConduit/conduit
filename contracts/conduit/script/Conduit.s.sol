7c6c068767f94f4b7f7f5d0cfaced44290327a4e
CMEONE <kabir@ramzan.me>
2026-02-20 16:26:55 -0700

    Format contracts

--- contracts/conduit/script/Conduit.s.sol
+++ contracts/conduit/script/Conduit.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-import { Script, console } from "forge-std/Script.sol";
-import { Conduit } from "../src/Conduit.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {Conduit} from "../src/Conduit.sol";
 
 contract ConduitScript is Script {
     Conduit public conduit;
--- contracts/conduit/test/Conduit.t.sol
+++ contracts/conduit/test/Conduit.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-import { Test, console } from "forge-std/Test.sol";
-import { Conduit } from "../src/Conduit.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {Conduit} from "../src/Conduit.sol";
 
 contract ConduitTest is Test {
     Conduit public conduit;
